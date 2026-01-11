be629616a6d9f8ba03a7d3040308cb50e0a7ade2
Richie Zhou <r631137986@gmail.com>
2026-01-10 17:47:13 -0800

    upload project

---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from 'react-router'
+
+import { RootLayout } from '@/routes/layouts/root-layout'
+import { EditorPage } from '@/routes/editor/page'
+import { PrintPage } from '@/routes/print/page'
+import { NotFoundPage } from '@/routes/not-found/page'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <EditorPage />,
+      },
+      { path: 'print', element: <PrintPage /> },
+      { path: '*', element: <NotFoundPage /> },
+    ],
+  },
+])
